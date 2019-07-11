1def68ab8fa0a25a818b0960b6593891aac5404b
Vadim <44911028+webqatutor@users.noreply.github.com>
2019-07-10 21:01:21 -0400

    Development (#3)
    * add nav bar to application.html.erb
    
    * put places in a box
    
    * add styles for navbar and background image
    
    * add custom font for brand name

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Nomster
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
