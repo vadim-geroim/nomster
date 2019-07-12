2a5afd2e5c7d36b10570bef5ee6ea8935bcd4e7b
Vadim <44911028+webqatutor@users.noreply.github.com>
2019-07-12 19:32:18 -0400

    Development (#4)
    * add nav bar to application.html.erb
    
    * put places in a box
    
    * add styles for navbar and background image
    
    * add custom font for brand name
    
    * add pagination to index page
    
    * add simple form to new page
    
    * map pagination to div and add padding-bottom
    
    * add create method to places controller
    
    * add links to navbar
    
    * add font-awesome gem and plus icon to navbar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,20 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all
+		@places = Place.paginate(page: params[:page], per_page:5)
+	end
+
+	def new
+		@place = Place.new
+	end	
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
 	end
 end
