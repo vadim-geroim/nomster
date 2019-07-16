8baa603a1fdc2956ad5590d0ea9c6fc64c5da4f7
vgeroim <vgeroim.qa@gmail.com>
2019-07-16 02:16:28 +0000

    add destroy btn to show places page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,6 +26,12 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 		@place.update_attributes(place_params)
 		redirect_to root_path
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
+		redirect_to root_path
 	end	
 
 	private
