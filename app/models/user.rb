01541d0982c0f5d2d0aeea4a6ee3b782771da763
vgeroim <vgeroim.qa@gmail.com>
2019-07-27 02:21:14 +0000

    add photo feature to place details page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :places   
-  has_many :comments    
+  has_many :comments
+  has_many :photos    
 end
