55bcfc925333095219eb2c33fcbd30ee1e0b8df8
vgeroim <vgeroim.qa@gmail.com>
2019-08-02 01:12:25 +0000

    send email notification to place's owner when comment saved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+# Action Mailer Configuration for Gmail  
+# config.action_mailer.delivery_method = :test #:smtp
 
+#   config.action_mailer.smtp_settings = {
+#     address: "smtp.gmail.com",
+#     port: 587,
+#     domain: "example.com",
+#     authentication: "plain",
+#     enable_starttls_auto: true,
+#     user_name: ENV['GMAIL_ADDRESS'],
+#     password: ENV['GMAIL_PASSWORD']
+#   }
 
 config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
   # In the development environment your application's code is reloaded on
@@ -44,7 +45,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
