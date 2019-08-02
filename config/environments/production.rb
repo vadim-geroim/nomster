8b05b70705e79e3cf77c37e49b72cd25cb84120b
vgeroim <vgeroim.qa@gmail.com>
2019-08-02 01:49:41 +0000

    add ActionMailer to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # Configure ActionMailer to Use SendGrid
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.default_url_options = { host: 'nomster-vadim-geroim.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
